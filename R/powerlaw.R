d5315bf649513a8be786038452734120912bfbdc
Erin Feichtinger <efeichtinger@mail.usf.edu>
2016-04-26 11:47:51 -0400

    Added information on the parameters, added an example and a reference

--- R/powerlaw.R
+++ R/powerlaw.R
@@ -1,18 +1,28 @@
 #' @title Powerlaw function for absolute growth rate (change in mass over time)
 #' 
 #' @description
-#' \code{PowerlawAGR}  returns the absolute growth rate following a powerlaw/allometric functional form
+#' \code{PowerlawAGR}  returns the absolute growth rate following a power law/allometric functional form
 #' 
-#'
 #' 
-#' @param r a number or numeric vector
-#' @param M biomass a number or numeric vector
-#' @param B a number or numeric vector
-#' @return numeric vector of the absolute growth rate following the powerlaw form rM^B
+#' @param r allometric constant, a number or numeric vector
+#' @param M biomass (kg/g), a number or numeric vector
+#' @param B scaling exponent, a number or numeric vector
+#' @return a number or numeric vector of the absolute growth rate following the power law 
+#' 
+#' @examples
+#' #Change in mass over time
+#' agr <- PowerlawAGR(0.25,100,0.4)
+#' agr 
+#' 3.62
+#' 
+#' @references
+#' C.E.T. Paine, T.R. Marthews, D.R. Vogt, D. Purves, M. Rees, A. Hector, and
+#' L.A. Turnbull. 2012. How to fit nonlinear growth models and calculate growth
+#' rates: an update for ecologists. Methods in Ecology and Evolution 3:245-256
 
 
-PowerlawAGR <- function(r,M,B)
-{
+PowerlawAGR <- function(r,M,B){
+
   AGR <- (r*M)^B
   return(AGR)
   
