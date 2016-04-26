17a43b49c0e5955719dde8dbbf743a155af61794
Erin Feichtinger <efeichtinger@mail.usf.edu>
2016-04-26 13:42:36 -0400

    Standarized code and documentation

--- R/monoBM.R
+++ R/monoBM.R
@@ -1,20 +1,24 @@
-#' @title mono.mass    
-#' @param r a number or numeric vector
-#' @param M M0 initial biomass, a number or numeric vector
-#' @param K, upper horizontal asymptote, a number or numeric vector
-#' @param t, time, a number or numeric vector 
-#' @return Biomass at time t for the monomolecular model of growth
+#' @title Mass at time t following the monomolecular function
+#' 
+#' @description
+#' \code{MonoMass} returns biomass at time t 
+#' 
+#' @param r allometric constant, a number or numeric vector
+#' @param M intial biomass, a number or numeric vector
+#' @param K upper horizontal asymptote, a number or numeric vector
+#' @param t time a number or numeric vector
+#' @return number or numeric vector, biomass at time t from the power law function
 #' 
 #' 
-mono.mass <- function(r,M,K,t)
+#' 
+#' @references
+#' C.E.T. Paine, T.R. Marthews, D.R. Vogt, D. Purves, M. Rees, A. Hector, and
+#' L.A. Turnbull. 2012. How to fit nonlinear growth models and calculate growth
+#' rates: an update for ecologists. Methods in Ecology and Evolution 3:245-256
+
+MonoMass <- function(r,M,K,t)
 {
-  mass <- K - exp(-r*t)*(K-M)
-  return(mass)
+  biomass <- K - exp(-r*t)*(K-M)
+  return(biomass)
   
 }
-
-#' References
-#' Paine, C. E. T., T. R. Marthews, D. R. Vogt, D. Purves, M. Rees, A. Hector
-#' and L. A. TUrnbull. 2012. How to fit nonlinear plant growth models and 
-#' calculate growth rates: an update for ecologists. Methods in Ecology and Evolution
-#' 3:245-256.
